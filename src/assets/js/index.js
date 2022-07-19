6f0a9d33783f71e7c543f7985622ee3523a02974
smiledie <i@smiledie-hub.ru>
2022-07-20 02:49:11 +0500

    fixes

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,7 @@ import "./fixes"
 import Router from './utils/Router';
 import common from './routes/common';
 import home from './routes/home';
+import map from '@images/map.png';
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
@@ -13,4 +14,27 @@ const routes = new Router({
 
 window.addEventListener("DOMContentLoaded", () => {
     routes.loadEvents()
+
+
+    ymaps.ready(function () {
+        const myMap = new ymaps.Map('map', {
+                center: [56.820282, 60.647613],
+                zoom: 18,
+                type: 'yandex#map',
+                controls: []
+            }, {
+            }),
+
+            myPlacemark = new ymaps.Placemark([56.820282, 60.649613], {
+                hintContent: '620100, Екатеринбург, Сибирский тракт, дом 12, строение 4, помещение 8'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: map,
+                iconImageSize: [62, 55],
+                iconImageOffset: [-31, -26]
+            });
+
+        myMap.geoObjects
+            .add(myPlacemark)
+    });
 })
